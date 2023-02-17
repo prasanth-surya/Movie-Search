357f6bed2eb0789b154cbbe301648358bb09f363
Prasanth Remella <98454591+prasanth-surya@users.noreply.github.com>
2023-02-17 10:21:36 +0530

    Update searchMovies.js

--- searchMovies.js
+++ searchMovies.js
@@ -7,10 +7,13 @@ export default function SearchMovies(){
     //create the state for movies, and update that state appropriate
     const [movies, setMovies] = useState([]);
     
+    
+    //async function
     const searchMovies = async (e) => {
         e.preventDefault();
                 
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&l
+        anguage=en-US&query=${query}&page=1&include_adult=false`;
         
         try {
             const res = await fetch(url);
